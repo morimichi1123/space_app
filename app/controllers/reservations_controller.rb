e03112d62eda031f5384b4cd0916fb382298282f
morimichi1123 <morisaki.michiya@lmi.ne.jp>
2019-11-20 02:43:17 +0000

    ここのuserの予約表示前

--- app/controllers/reservations_controller.rb
+++ app/controllers/reservations_controller.rb
@@ -1,7 +1,12 @@
 class ReservationsController < ApplicationController
+  before_action :logged_in_user
+
+  def new
+    @reservation = Reservation.new
+  end
 
   def show
-    @reservation = Reservation.find(params[:id])
+    @reservation = Reservation.find(reservation_params[:id])
   end
 
   def list
@@ -11,6 +16,7 @@ class ReservationsController < ApplicationController
   def create
     @reservation = current_user.reservations.create(reservation_params)
     #debugger
+    #@reservation = Reservation.new(reservation_params)
     if @reservation.save
         flash[:success] = "予約が完了しました"
         redirect_to list_path
@@ -19,13 +25,13 @@ class ReservationsController < ApplicationController
         #@space = Space.new(space_params)
         #redirect_to show_path
         flash[:danger] = "日付を入力してください"
-        render 'reservations/show'
+        render 'spaces/show'
       end
   end
 
   private
   def reservation_params
-    params.require(:reservation).permit(:start_date, :end_date, :space_id)
+    params.require(:reservation).permit(:user_id, :space_id, :start_date, :end_date)
   end
 
  #def space_params
